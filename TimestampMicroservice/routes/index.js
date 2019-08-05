a508503f4bcf6d330f69a94a7a6da13c2566bf5a
Ariel Lorenzo <ariellorenzo08@gmail.com>
2019-08-05 15:42:38 -0400

    Able to check for invalid date the right way

--- TimestampMicroservice/routes/index.js
+++ TimestampMicroservice/routes/index.js
@@ -3,16 +3,23 @@ var express = require('express');
 var router = express.Router();
 var url = require('url');
 
-/* GET home page. */
 router.get('/api/timestamp/:date_string', function (req, res) {
+    // Getting the parameter string
     let date_string = req.param("date_string");
+
+    // Check if the received date is in milliseconds
     if (isNaN(date_string) === true)
         var dt = new Date(date_string);
     else dt = new Date(parseInt(date_string));
-    res.send({ unix: dt.getTime(), utc: dt.toUTCString() });
+
+    // Checking if the date is valid
+    if (!isNaN(dt.getTime()))
+        res.send({ unix: dt.getTime(), utc: dt.toUTCString() });
+    else res.send({ error: "Invalid Date" });
 });
 
 router.get('/api/timestamp/', function (req, res) {
+    // Return today's date
     res.send({ unix: (new Date).getTime(), utc: (new Date).toUTCString() });
 });
 
